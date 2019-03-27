aead356f348f48d64bbe7ea72a8201ec27e17c95
Park-JunHyung <schwazodiac@gmail.com>
2019-03-27 20:19:03 +0900

    Seperate each question entities

--- programmers/Solution.java
+++ programmers/Solution.java
@@ -3,14 +3,21 @@ package programmers;
 public class Solution {
     public static void main(String[] args) {
 
+//        runHash01();
+        runHash02();
+    }
+    static void runHash01(){
         hash01 hash01 = new hash01();
-        System.out.println("Hash01 testcase 01");
+
+        System.out.println("Hash01 testcase 1");
         String[] participant = {"leo", "kiki", "eden", "leo"};
         String[] completion = {"kiki", "eden", "leo"};
         System.out.println(hash01.solution(participant, completion));
-
+    }
+    static void runHash02(){
         hash02 hash02 = new hash02();
-        System.out.println("Hash02 testcase 01");
+
+        System.out.println("Hash02 testcase 1");
         String[] phone_book = {"119", "97674223", "1195524421"};
         System.out.println(hash02.solution(phone_book));
     }
--- programmers/hash02.java
+++ programmers/hash02.java
@@ -2,7 +2,13 @@ package programmers;
 
 class hash02 {
     public boolean solution(String[] phone_book) {
+        long before =System.currentTimeMillis();
+
         boolean answer = true;
+
+
+        long after=System.currentTimeMillis()-before;
+        System.out.println("Estimated Time : "+after+"ms");
         return answer;
     }
 }
