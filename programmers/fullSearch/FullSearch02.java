1fb9df3a9cb9908a204f700c6bcf737788e04a1d
Park-JunHyung <schwazodiac@gmail.com>
2019-04-18 22:33:53 +0900

    Solved FullSearch02 question..

--- programmers/fullSearch/FullSearch02.java
+++ programmers/fullSearch/FullSearch02.java
@@ -8,31 +8,38 @@ import java.util.Iterator;
 public class FullSearch02 {
     public int solution(String numbers) {
         int answer = 0;
-        HashSet<Integer> numberSet=new HashSet<>();
-        String [] numbersArray= numbers.split("");
-        for (int i=1;i<=numbersArray.length;i++){
-            perm(numbersArray,0,numbersArray.length,i,numberSet);
+        HashSet<Integer> numberSet = new HashSet<>();
+        String[] numbersArray = numbers.split("");
+        for (int i = 1; i <= numbersArray.length; i++) {
+            perm(numbersArray, 0, numbersArray.length, i, numberSet);
         }
         for (Iterator<Integer> it = numberSet.iterator(); it.hasNext(); ) {
+            boolean isPrime = true;
             Integer targetNumber = it.next();
-            if (targetNumber<2) continue;
-            for (int i=2; i<targetNumber; i++){
-
+            if (targetNumber < 2) continue;
+            if (targetNumber == 2) {
+                answer++;
+                continue;
             }
-
+            for (int i = 2; i < targetNumber; i++) {
+                if (targetNumber % i == 0) {
+                    isPrime = false;
+                    continue;
+                }
+            }
+            if (isPrime) answer++;
         }
         return answer;
     }
 
     public static void perm(String[] arr, int depth, int n, int k, HashSet set) {
         if (depth == k) {
-//            System.out.println(print(arr, k));
             set.add(print(arr, k));
             return;
         }
         for (int i = depth; i < n; i++) {
             swap(arr, i, depth);
-            perm(arr, depth + 1, n, k,set);
+            perm(arr, depth + 1, n, k, set);
             swap(arr, i, depth);
         }
     }
@@ -44,9 +51,9 @@ public class FullSearch02 {
     }
 
     public static int print(String[] arr, int k) {
-        String tmp="";
+        String tmp = "";
         for (int i = 0; i < k; i++) {
-            tmp+=arr[i];
+            tmp += arr[i];
         }
         return Integer.parseInt(tmp);
     }
